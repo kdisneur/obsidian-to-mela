d875682b52080b3c6fb0ac79953f422431b575bc
Kevin Disneur <kevin@disneur.me>
2024-04-14 10:17:14 +0200

    Add all mela recipe fields, not just the one needed for export

--- mela/recipe.go
+++ mela/recipe.go
@@ -4,10 +4,15 @@ type Recipe struct {
 	ID              string   `json:"id"`
 	Title           string   `json:"title"`
 	Categories      []string `json:"categories"`
+	Text            string   `json:"text"`
+	Images          []string `json:"images"`
 	Yield           string   `json:"yield"`
 	PreparationTime string   `json:"prepTime"`
 	CookingTime     string   `json:"cookTime"`
 	TotalTime       string   `json:"totalTime"`
 	Ingredients     string   `json:"ingredients"`
 	Instructions    string   `json:"instructions"`
+	Notes           string   `json:"notes"`
+	Nutrition       string   `json:"nutrition"`
+	Link            string   `json:"link"`
 }
